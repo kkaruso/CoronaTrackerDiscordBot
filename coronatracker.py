7bf3e6bc1a7a5e9cf8f3e77ad603d7d3f1c25b80
Darwin Schulz <darwinschulz@outlook.com>
2021-07-24 22:34:53 +0200

    Refactore code

--- coronatracker.py
+++ coronatracker.py
@@ -1,44 +1,52 @@
-import requests, discord
-from tokenDC import DISCORDTOKEN
+import requests
+import discord
+from tokenDC import DISCORDTOKEN, CITIES
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-
-    newJsonCity = coronaCity()
-    newJsonGermany = coronaGermany()
 
     if message.author == client.user:
         return
 
     if message.content.startswith('!corona'):
-        test = message.content.split(maxsplit=1)[1]
-        city = ""
-        for _, value in newJsonCity['data'].items():
-            if value['name'] == test:
-                city = value
-        await message.channel.send(f"{test} neue Fälle: {city['delta']['cases']} \n{test} R-Wert: {round(city['weekIncidence'], 2)}")
-    
-    if message.content.startswith('deutschland'):
-        await message.channel.send(f"Deutschland neue Fälle: {newJsonGermany['delta']['cases']} \nDeutschland R-Wert: {round(newJsonGermany['weekIncidence'], 2)}")
+        user_request = message.content.split(maxsplit=1)[1]
+        print(user_request.lower() in CITIES)
+        if user_request.lower() == 'help':
+            await help(message)
+        elif user_request.lower() == 'germany':
+            await corona_germany(message)
+        elif user_request.lower() in CITIES:
+            await corona_city(message, user_request)
 
-def coronaCity():
 
-    r = requests.get('https://api.corona-zahlen.org/districts').json()
-    newJsonCity = r
+async def corona_city(message, requested_city):
+    data_cities = requests.get(
+        'https://api.corona-zahlen.org/districts').json()['data']
+    requested_city_lower = requested_city.lower()
 
-    return newJsonCity
+    for _, data_city in data_cities.items():
+        if data_city['name'].lower() == requested_city_lower:
+            await message.channel.send(f"{requested_city} neue Fälle: {data_city['delta']['cases']} \n{requested_city} R-Wert: {round(data_city['weekIncidence'], 2)}")
+            return
 
-def coronaGermany():
+    await message.channel.send(f"Could not retrive any data for {requested_city}")
 
-    r = requests.get('https://api.corona-zahlen.org/germany').json()
-    newJsonGermany = r
 
-    return newJsonGermany
+async def corona_germany(message):
+    data_germany = requests.get('https://api.corona-zahlen.org/germany').json()
+
+    await message.channel.send(f"Deutschland neue Fälle: {data_germany['delta']['cases']} \nDeutschland R-Wert: {round(data_germany['weekIncidence'], 2)}")
+
+
+async def help(message):
+    await message.channel.send(f"No help")
 
 client.run(DISCORDTOKEN)
